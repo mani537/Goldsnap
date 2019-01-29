273ba02a51e783c076e0f254c653c8ab211e2df5
mani <manikantareddym007@gmail.com>
2019-01-29 12:25:20 +0530

    revert the change

--- GoldSnap/ScreenShot/src/FolderScreation.java
+++ GoldSnap/ScreenShot/src/FolderScreation.java
@@ -9,7 +9,7 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
-public class FolderScreation2
+public class FolderScreation
 {
   String excelFileNamefinal;
   String text = "Test";
