ff7cf0b20c4944f86282f1aa5a79d6666c01889e
mani <manikantareddym007@gmail.com>
2019-01-29 12:21:36 +0530

    Name change for the java file

--- GoldSnap/ScreenShot/src/FolderScreation.java
+++ GoldSnap/ScreenShot/src/FolderScreation.java
@@ -9,7 +9,7 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
-public class FolderScreation
+public class FolderScreation2
 {
   String excelFileNamefinal;
   String text = "Test";
